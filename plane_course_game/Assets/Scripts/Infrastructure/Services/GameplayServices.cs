dc1ede8137ccaabf626c287ca339dda88980ecf8
Naor <naorberger@gmail.com>
2021-10-11 14:05:29 +0300

    msuci + sfx

--- plane_course_game/Assets/Scripts/Infrastructure/Services/GameplayServices.cs
+++ plane_course_game/Assets/Scripts/Infrastructure/Services/GameplayServices.cs
@@ -36,6 +36,7 @@ namespace Infrastructure.Services
 
             var gcgo = new GameObject("GameCore");
             _gameCore = gcgo.AddComponent<GameCore>();
+            
         }
 
         #endregion
